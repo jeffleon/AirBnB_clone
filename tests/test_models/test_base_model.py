fb3733527cb4b403a41120b98e2da9a0d5b1bf01
SneyderHOL <1507@holbertonschool.com>
2020-06-30 14:34:27 +0000

    [UPDATE]: pep8 to test files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,8 +6,6 @@ import json
 import re
 from models import base_model
 from models.base_model import BaseModel
-#from models.rectangle import Rectangle
-#from models.square import Square
 
 
 class TestBaseModel(unittest.TestCase):
@@ -45,65 +43,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(re.match(pattern, base_obj.id))
         self.assertIsNotNone(re.match(datetime_patt, base_obj.created_at))
         self.assertIsNotNone(re.match(datetime_patt, base_obj.updated_at))
-
-        base_obj2 = Base(9)
-        self.assertEqual(base_obj2.id, 9)
-        base_obj3 = Base('a')
-        self.assertEqual(base_obj3.id, 'a')
-        base_obj4 = Base()
-        self.assertEqual(base_obj4.id, 2)
-        with self.assertRaises(TypeError):
-            base_obj5 = Base(1, 3)
-
-    def test_save_to_file(self):
-        """Tests for save_to_file method"""
-        rectangle_obj = Rectangle(9, 8, 1, 2, 23)
-        square_obj = Square(4, 5, 6, 21)
-        Base.save_to_file([rectangle_obj, square_obj])
-        with open('Base.json', mode='r', encoding='utf-8') as n_file:
-            f_cont = json.load(n_file)
-            self.assertEqual(type(f_cont), list)
-            self.assertEqual(len(f_cont), 2)
-        with self.assertRaises(TypeError):
-            Base.save_to_file()
-
-    def test_load_from_file(self):
-        """Tests for load_from_file method"""
-        pass
-
-    def test_to_json_string(self):
-        """Tests for to_json_string method"""
-        res = Base.to_json_string([])
-        self.assertEqual(type(res), str)
-        self.assertEqual(Base.to_json_string(None), '[]')
-        self.assertEqual(Base.to_json_string(1), '1')
-        self.assertEqual(Base.to_json_string([]), '[]')
-        self.assertEqual(Base.to_json_string([{}]), '[{}]')
-        self.assertEqual(Base.to_json_string([1]), '[1]')
-        self.assertEqual(Base.to_json_string([[1, 2]]), '[[1, 2]]')
-        self.assertEqual(Base.to_json_string([(True, 2)]), '[[true, 2]]')
-        self.assertEqual(Base.to_json_string([{'id': 3}]), '[{"id": 3}]')
-        with self.assertRaises(TypeError):
-            Base.to_json_string()
-        with self.assertRaises(TypeError):
-            Base.to_json_string([], [])
-
-    def test_from_json_string(self):
-        """Tests for from_json_string method"""
-        with self.assertRaises(TypeError):
-            Base.from_json_string()
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string(''), [])
-        self.assertEqual(Base.from_json_string('[]'), [])
-        self.assertEqual(Base.from_json_string(1), None)
-        self.assertEqual(Base.from_json_string((1, 2)), None)
-        self.assertEqual(Base.from_json_string(True), None)
-        self.assertEqual(Base.from_json_string('[{}]'), [{}])
-        self.assertEqual(Base.from_json_string('[{"id": 3}]'), [{'id': 3}])
-
-    def test_create(self):
-        """Tests for create method"""
-        pass
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,8 +5,6 @@ import pep8
 import json
 from models import user
 from models.user import User
-#from models.rectangle import Rectangle
-#from models.square import Square
 
 
 class TestUser(unittest.TestCase):
@@ -31,70 +29,19 @@ class TestUser(unittest.TestCase):
         result_test_user = pep8_val.check_files([test_user_path])
         self.assertEqual(result_test_user.total_errors, 0)
 
-*********
     def test_new_object(self):
         """Tests when an instance is created"""
-        base_obj = Base()
-        self.assertIsInstance(base_obj, Base)
-        self.assertEqual(base_obj.id, 1)
-        base_obj2 = Base(9)
-        self.assertEqual(base_obj2.id, 9)
-        base_obj3 = Base('a')
-        self.assertEqual(base_obj3.id, 'a')
-        base_obj4 = Base()
-        self.assertEqual(base_obj4.id, 2)
-        with self.assertRaises(TypeError):
-            base_obj5 = Base(1, 3)
-
-    def test_save_to_file(self):
-        """Tests for save_to_file method"""
-        rectangle_obj = Rectangle(9, 8, 1, 2, 23)
-        square_obj = Square(4, 5, 6, 21)
-        Base.save_to_file([rectangle_obj, square_obj])
-        with open('Base.json', mode='r', encoding='utf-8') as n_file:
-            f_cont = json.load(n_file)
-            self.assertEqual(type(f_cont), list)
-            self.assertEqual(len(f_cont), 2)
-        with self.assertRaises(TypeError):
-            Base.save_to_file()
-
-    def test_load_from_file(self):
-        """Tests for load_from_file method"""
-        pass
-
-    def test_to_json_string(self):
-        """Tests for to_json_string method"""
-        res = Base.to_json_string([])
-        self.assertEqual(type(res), str)
-        self.assertEqual(Base.to_json_string(None), '[]')
-        self.assertEqual(Base.to_json_string(1), '1')
-        self.assertEqual(Base.to_json_string([]), '[]')
-        self.assertEqual(Base.to_json_string([{}]), '[{}]')
-        self.assertEqual(Base.to_json_string([1]), '[1]')
-        self.assertEqual(Base.to_json_string([[1, 2]]), '[[1, 2]]')
-        self.assertEqual(Base.to_json_string([(True, 2)]), '[[true, 2]]')
-        self.assertEqual(Base.to_json_string([{'id': 3}]), '[{"id": 3}]')
-        with self.assertRaises(TypeError):
-            Base.to_json_string()
-        with self.assertRaises(TypeError):
-            Base.to_json_string([], [])
-
-    def test_from_json_string(self):
-        """Tests for from_json_string method"""
-        with self.assertRaises(TypeError):
-            Base.from_json_string()
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string(''), [])
-        self.assertEqual(Base.from_json_string('[]'), [])
-        self.assertEqual(Base.from_json_string(1), None)
-        self.assertEqual(Base.from_json_string((1, 2)), None)
-        self.assertEqual(Base.from_json_string(True), None)
-        self.assertEqual(Base.from_json_string('[{}]'), [{}])
-        self.assertEqual(Base.from_json_string('[{"id": 3}]'), [{'id': 3}])
-
-    def test_create(self):
-        """Tests for create method"""
-        pass
+        user_obj = User()
+        self.assertIsInstance(user_obj, User)
+        self.assertEqual(type(user_obj.id), str)
+        self.assertEqual(type(user_obj.created_at), datetime.datetime)
+        self.assertEqual(type(user_obj.updated_at), datetime.datetime)
+        pattern = '[0-9]*, [0-9]*, [0-9]*, [0-9]*, [0-9]*, [0-9]*, [0-9]*'
+        pattern_t = '[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*.[0-9]*'
+        datetime_patt = 'datetime.datetime(' + pattern_t + ')'
+        self.assertIsNotNone(re.match(pattern, user_obj.id))
+        self.assertIsNotNone(re.match(datetime_patt, user_obj.created_at))
+        self.assertIsNotNone(re.match(datetime_patt, user_obj.updated_at))
 
 if __name__ == '__main__':
     unittest.main()
