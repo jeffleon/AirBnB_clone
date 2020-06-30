381f44f3678a0a9e736802ce1b8e2293db946598
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-30 17:36:32 +0000

    fix tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,6 +5,8 @@ import pep8
 import json
 from models import city
 from models.city import City
+import datetime
+import re
 
 
 class TestCity(unittest.TestCase):
@@ -32,12 +34,11 @@ class TestCity(unittest.TestCase):
         self.assertEqual(type(city_obj.id), str)
         self.assertEqual(type(city_obj.created_at), datetime.datetime)
         self.assertEqual(type(city_obj.updated_at), datetime.datetime)
-        pattern = '[0-9]*, [0-9]*, [0-9]*, [0-9]*, [0-9]*, [0-9]*, [0-9]*'
-        pattern_t = '[0-9]*-[0-9]*-[0-9]*T[0-9]*:[0-9]*:[0-9]*.[0-9]*'
-        datetime_patt = 'datetime.datetime(' + pattern_t + ')'
+        pattern = '[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*'
+        datetime_patt = '[0-9]*-[0-9]*-[0-9]* [0-9]*:[0-9]*:[0-9]*.[0-9]*'
         self.assertIsNotNone(re.match(pattern, city_obj.id))
-        self.assertIsNotNone(re.match(datetime_patt, city_obj.created_at))
-        self.assertIsNotNone(re.match(datetime_patt, city_obj.updated_at))
+        self.assertIsNotNone(re.match(datetime_patt, str(city_obj.created_at)))
+        self.assertIsNotNone(re.match(datetime_patt, str(city_obj.updated_at)))
 
 if __name__ == '__main__':
     unittest.main()
