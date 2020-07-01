934815746b59cf1c129570455f1a677d757660b8
SneyderHOL <1507@holbertonschool.com>
2020-07-01 16:04:32 +0000

    [UPDATE]: remove methods in test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,22 +2,10 @@
 """Unittest for FileStorage"""
 import unittest
 import pep8
-import datetime
-from models.engine import file_storage
-from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """Class for testing with unit test the FileStorage class"""
-
-    def test_docstrings(self):
-        """Tests for dosctrings"""
-        self.assertGreater(len(file_storage.__doc__), 1)
-        self.assertGreater(len(FileStorage.__doc__), 1)
-        self.assertGreater(len(FileStorage.all.__doc__), 1)
-        self.assertGreater(len(FileStorage.new.__doc__), 1)
-        self.assertGreater(len(FileStorage.save.__doc__), 1)
-        self.assertGreater(len(FileStorage.reload.__doc__), 1)
 
     def test_pep8(self):
         """ Test for PEP8 """
@@ -28,11 +16,6 @@ class TestFileStorage(unittest.TestCase):
         t_file_st_path = 'tests/test_models/test_engine/test_file_storage.py'
         result_test_file_storage = pep8_val.check_files([t_file_st_path])
         self.assertEqual(result_test_file_storage.total_errors, 0)
-
-    def test_new_object(self):
-        """Tests when an instance is created"""
-        file_s_obj = FileStorage()
-        self.assertIsInstance(file_s_obj, FileStorage)
 
 if __name__ == '__main__':
     unittest.main()
