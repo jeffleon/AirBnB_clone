21c155fb1ac8a1c1534d0d4182ed66c51de4799b
SneyderHOL <1507@holbertonschool.com>
2020-07-01 16:23:11 +0000

    [UPDATE]: empty test for base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 """Unittest for BaseModel"""
 import unittest
-import pep8
 
 
 class TestBaseModel(unittest.TestCase):
     """Class for testing with unit test the BaseModel class"""
-
-    def test_pep8(self):
-        """ Test for PEP8 """
-        pep8_val = pep8.StyleGuide(quiet=True)
-        base_model_path = 'models/base_model.py'
-        result_base_model = pep8_val.check_files([base_model_path])
-        self.assertEqual(result_base_model.total_errors, 0)
-        test_base_model_path = 'tests/test_models/test_base_model.py'
-        result_test_base_model = pep8_val.check_files([test_base_model_path])
-        self.assertEqual(result_test_base_model.total_errors, 0)
+    pass
 
 if __name__ == '__main__':
     unittest.main()
