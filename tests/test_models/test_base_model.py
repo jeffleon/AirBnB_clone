05438d8e117ae58b1b8cf8c5cef8f3f5bf7ccc42
SneyderHOL <1507@holbertonschool.com>
2020-07-01 06:18:08 +0000

    [UPDATE]: new content

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,11 +2,6 @@
 """Unittest for BaseModel"""
 import unittest
 import pep8
-import json
-import re
-from models import base_model
-from models.base_model import BaseModel
-import datetime
 
 
 class TestBaseModel(unittest.TestCase):
@@ -16,10 +11,6 @@ class TestBaseModel(unittest.TestCase):
         """Tests for dosctrings"""
         self.assertGreater(len(base_model.__doc__), 1)
         self.assertGreater(len(BaseModel.__doc__), 1)
-        self.assertGreater(len(BaseModel.__init__.__doc__), 1)
-        self.assertGreater(len(BaseModel.__str__.__doc__), 1)
-        self.assertGreater(len(BaseModel.save.__doc__), 1)
-        self.assertGreater(len(BaseModel.to_dict.__doc__), 1)
 
     def test_pep8(self):
         """ Test for PEP8 """
@@ -27,8 +18,8 @@ class TestBaseModel(unittest.TestCase):
         base_model_path = 'models/base_model.py'
         result_base_model = pep8_val.check_files([base_model_path])
         self.assertEqual(result_base_model.total_errors, 0)
-        test_base_model_path = 'tests/test_models/test_base_model.py'
-        result_test_base_model = pep8_val.check_files([test_base_model_path])
+        tbase_path = 'tests/test_models/test_base_model.py'
+        result_test_base_model = pep8_val.check_files([tbase_path])
         self.assertEqual(result_test_base_model.total_errors, 0)
 
 
